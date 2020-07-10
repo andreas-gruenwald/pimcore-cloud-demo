e0aa3da2db3e82b47dadc3cca63a5e5974714096
agruenwald <a.gruenw@gmail.com>
2020-07-10 09:13:35 +0200

    Install AWS S3 library.

--- app/startup.php
+++ app/startup.php
@@ -1,21 +1,23 @@
 <?php
 use Aws\S3\S3Client;
-if (getenv('s3EngineEnabled')) {
 
-    $s3Client = new S3Client([
-        'version' => 'latest',
-        'region' => getenv('s3Region'), //'us-east-2', // choose your favorite region
-        'credentials' => [
-            // use your aws credentials
-            'key' => getenv('s3Key'), //'AKIAJOAFDIFXXXXXXXXXX',
-            'secret' => getenv('s3Secret'), //'uw7fGn0if9KvQR09O+n7E8+XXXXXXXXXX',
-        ],
-    ]);
+if (!getenv('s3EngineEnabled')) {
+    return;
+}
 
-    $s3Client->registerStreamWrapper();
+$s3Client = new S3Client([
+    'version' => 'latest',
+    'region' => getenv('s3Region'), //'us-east-2', // choose your favorite region
+    'credentials' => [
+        // use your aws credentials
+        'key' => getenv('s3Key'), //'AKIAJOAFDIFXXXXXXXXXX',
+        'secret' => getenv('s3Secret'), //'uw7fGn0if9KvQR09O+n7E8+XXXXXXXXXX',
+    ],
+]);
+
+$s3Client->registerStreamWrapper();
 
 // set default file context
-    \Pimcore\File::setContext(stream_context_create([
-        's3' => ['seekable' => true]
-    ]));
-}
+\Pimcore\File::setContext(stream_context_create([
+    's3' => ['seekable' => true]
+]));

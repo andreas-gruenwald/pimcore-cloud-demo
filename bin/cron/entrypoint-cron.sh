8f20fdfc918a568bbd9967df3a9c5f544f13f53e
agruenwald <a.gruenw@gmail.com>
2020-08-05 08:37:55 +0200

    Crontab support.

--- bin/cron/entrypoint-cron.sh
+++ bin/cron/entrypoint-cron.sh
@@ -6,15 +6,30 @@ set -e
 #
 
 echo "CHECK IF CLI IS ENABLED..."
-echo "::::${CLI_ENABLED}".
 if [ "$CLI_ENABLED" = 'true' ]; then
     INPUT_ENV="dev"
     echo "CLI_ENABLED is true, installing crontab..."
-    sed "s/{env}/$INPUT_ENV/g" /var/www/html/bin/cron/crontab_definition | crontab -
-    echo "\n\n*************************\n crontab installed (${INPUT_ENV})! \n*************************\n";
-    crontab -l
-    echo "\n";
+
+    # for logging cron on docker, see https://github.com/moby/moby/issues/19616
+    mkdir /var/www/html/var/logs
+    touch /var/www/html/var/logs/cron.log
+    chown -R www-data:www-data /var/www/html/var/logs
+    ln -sf /proc/1/fd/1 /var/www/html/var/logs/cron.log
+
+    # let www-data user execute sudo without password, so that logs
+    # can be sent correctly.
+    echo "www-data ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+    # make environment variables available for crontab
+    printenv | grep -e "APP_ENV\|s3\|MYSQL\|REDIS" >> /etc/environment
+
     service cron start
+    #env > /var/www/html/.env
+    #chown www-data:www-data /var/www/html/.env
+    sed "s/{env}/$INPUT_ENV/g" /var/www/html/bin/cron/crontab_definition | crontab -u www-data -
+    echo "\n\n*************************\n crontab installed (${INPUT_ENV})! \n*************************\n";
+    crontab -u www-data -l
+    echo "\n";
 fi
 
 #exec "$@"
--- bin/cron/job/dev/every-5-minutes.sh
+++ bin/cron/job/dev/every-5-minutes.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # Pimcore - Maintenance
-~/www/bin/console pimcore:maintenance -j scheduledtasks,runSanityCheck
+${HOME}/html/bin/console pimcore:maintenance -j scheduledtasks,runSanityCheck
 
 # Process Manager Maintenance
 #~/www/bin/console process-manager:maintenance
 
 # Ecommerce Queue Processing
-~/www/bin/console ecommerce:indexservice:process-queue preparation
-~/www/bin/console ecommerce:indexservice:process-queue update-index
+${HOME}/html//bin/console ecommerce:indexservice:process-queue preparation
+${HOME}/html/bin/console ecommerce:indexservice:process-queue update-index

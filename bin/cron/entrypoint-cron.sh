32cf96cd2fe8f83c36ad68e3495cf0fd4beca35e
agruenwald <a.gruenw@gmail.com>
2020-08-06 09:12:38 +0200

    Support packaging environment variables inside image.

--- bin/cron/entrypoint-cron.sh
+++ bin/cron/entrypoint-cron.sh
@@ -11,23 +11,31 @@ if [ "$CLI_ENABLED" = 'true' ]; then
     echo "CLI_ENABLED is true, installing crontab..."
 
     # for logging cron on docker, see https://github.com/moby/moby/issues/19616
+    echo "first, logs are setup..."
     mkdir /var/www/html/var/logs
     touch /var/www/html/var/logs/cron.log
     chown -R www-data:www-data /var/www/html/var/logs
+    echo "second, a symbolic link is set..."
     ln -sf /proc/1/fd/1 /var/www/html/var/logs/cron.log
 
+    echo "third, permissions are adapted..."
     # let www-data user execute sudo without password, so that logs
     # can be sent correctly.
     echo "www-data ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
+    echo "fourth, the /etc/environment is skpped, because AWS cannot deal with it..."
     # make environment variables available for crontab
-    printenv | grep -e "APP_ENV\|s3\|MYSQL\|REDIS" >> /etc/environment
+    #printenv | grep -e "APP_ENV\|s3\|MYSQL\|REDIS" >> /etc/environment
 
+    echo "fifth, cron is started as a service..."
     service cron start
     #env > /var/www/html/.env
     #chown www-data:www-data /var/www/html/.env
+
+    echo "sixth, crontab is installed..."
     sed "s/{env}/$INPUT_ENV/g" /var/www/html/bin/cron/crontab_definition | crontab -u www-data -
     echo "\n\n*************************\n crontab installed (${INPUT_ENV})! \n*************************\n";
     crontab -u www-data -l
     echo "\n";
+    echo "finally, the entrypoint-cron.sh script is exited..."
 fi
--- web/test-cli.php
+++ web/test-cli.php
@@ -11,30 +11,98 @@
 
     <style>
         body {
-            font-family: Helvetica, Arial, sans-serif;
+            font-family: monospace;
         }
 
         .console {
             background-color:black;
             color:white;
+            min-height: 3em;
         }
+
+        .colorbox {
+            min-height:50px;
+            line-height:50px;
+            font-weight:bold;
+            min-width:200px;
+            background-color: <?php echo getenv('APP_COLOR') ? : 'white' ;?>
+            padding-left:2em;
+            padding-right:2em;
+            text-align:center;
+            position: absolute;
+            right: 0px;
+            top: 0px;
+        }
+
+        .clibox {
+            padding:5px;
+            background-color:black;
+            color:white;
+            margin: 0 0 20px 0;
+            display: inline-block;
+        }
+
+        .clibox.enabled {
+            color:green;
+            font-weight:bold;
+            font-family: monospace;
+        }
+
     </style>
 </head>
 <body>
 
+<h1>Docker Toolbox for Hosted Containers</h1>
+
+<?php if (getenv('APP_COLOR')):?>
+    <div class="colorbox">Your app's color is <?php echo getenv('APP_COLOR');?>.</div>
+<?php endif; ?>
+
+<div class="clibox"><?php echo getenv('CLI_ENABLED') == 'true' ? 'CLI is enabled.' : 'CLI is deactivated.';?></div>
+
+
 <form method="POST" action="">
 
     Command: <input type="text" name="userInput" id="userInput" style="min-width:400px">
-    <input type="submit" value="Submit"/>
+    <input type="submit" value="Execute"/>
 
     <pre class="console"><?php
         if ($ui = $_POST['userInput']) {
             echo shell_exec($ui.' 2>&1');
+        } else {
+            echo shell_exec('whoami').'/sh:';
         }
         ?>
     </pre>
 
 </form>
+
+<?php
+$ecsMetaUrl = getenv('ECS_CONTAINER_METADATA_URI');
+if ($ecsMetaUrl) {
+    //@see https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task-metadata-endpoint-v3.html
+    $calls = [
+        'metaUrl' => sprintf('%s', $ecsMetaUrl),
+        'tasks' => sprintf('%s/task', $ecsMetaUrl),
+        'stats' => sprintf('%s/stats', $ecsMetaUrl),
+        'taskStats' => sprintf('%s/task/stats', $ecsMetaUrl),
+    ];
+
+    ?>
+
+    <div class="ecs-info">
+        <h2>AWS ECS provides a link to access metadata about the task and the container environment:</h2>
+        <?php foreach ($calls as $name => $command) {
+
+            echo '<h3>'.$name.'</h3>';
+            p_r(file_get_contents($url));
+        }
+        ?>
+    </div>
+
+    <?php
+}
+?>
 
 </body>
 </html>
